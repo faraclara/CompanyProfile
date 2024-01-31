3a1096300dc436f5a29fef5b25fed2271a5fbf16
faraclara <faraadevianaaa@gmail.com>
2024-01-31 14:00:10 +0700

    07 || Halaman About

--- resources/views/home/about/index.blade.php
+++ resources/views/home/about/index.blade.php
@@ -1,1 +1,17 @@
-<p>This is index for about</p>
+<div class="wrapper-hero">
+<img src="/img/banner.jpg" class="img-hero" width="100%" alt="">
+<div class="text-hero">
+    <h4><b>Tentang Kami</b></h4>
+</div>
+</div>
+
+<div class="container py-3">
+    <div class="row">
+        <div class="col-md-4">
+            <img src="/img/banner.jpg" width="100%" alt="">
+        </div>
+        <div class="col-md-8">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore, excepturi ex esse magni ratione error aperiam enim laboriosam voluptatibus tempora et atque nisi incidunt iste ipsa quo reprehenderit voluptas. Saepe!
+            </div>
+    </div>
+</div>
